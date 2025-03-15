8df04f7533e443e4747aa468ef345eb6d8d67180
侑介 <spark9487111@gmail.com>
2025-03-15 13:37:20 +0800

    update lib

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,134 +1,145 @@
-function _0x2dbe(_0x2cfd89, _0x36ac1a) {
-  const _0x35fd86 = _0x35fd();
-  return (
-    (_0x2dbe = function (_0x2dbe51, _0x5f2186) {
-      _0x2dbe51 = _0x2dbe51 - 0x1bd;
-      let _0x4ef444 = _0x35fd86[_0x2dbe51];
-      return _0x4ef444;
-    }),
-    _0x2dbe(_0x2cfd89, _0x36ac1a)
-  );
-}
-function _0x35fd() {
-  const _0x57bd4a = [
-    "test",
-    "324334MEBUWt",
-    "驗證碼格式錯誤！",
-    "data",
+function _0x370d() {
+  const _0x307497 = [
+    "password",
     "&lang=",
-    "1074310xIQosW",
-    "2726397sHhnbf",
-    "686008BPvNmC",
-    "error",
-    "textContent",
-    "arraybuffer",
-    "請輸入轉移碼",
-    "http://localhost:3000/download",
-    "input[name=\x22cc\x22]:checked",
-    "https://bc.godfat.org/?seed=",
+    "757872AFcDNV",
+    "227500FBxpdt",
     "message",
-    "Failed\x20to\x20Parse:\x20",
-    "請選擇遊戲版本",
-    "log",
-    "value",
-    "7jNWphC",
-    "getElementById",
-    "4313744TRsYrC",
-    "location",
-    "1847874VqyZEV",
-    "15Ncoqxw",
-    "2211377HTpVej",
     "seed",
-    "6MBrexJ",
+    "post",
+    "驗證碼格式錯誤！",
+    "請輸入轉移碼",
+    "95Lbctee",
+    "error",
+    "63vOHNfS",
+    "rareGachaSeed:",
+    "74140cmoveI",
     "Failed\x20to\x20Parse.\x20Please\x20Check\x20All\x20Fields",
-    "Seed:\x20",
+    "13OKduOm",
+    "請選擇遊戲版本",
+    "2294874ZQgnfM",
+    "860ArJBhY",
+    "log",
+    "434oKdXtw",
+    "data",
+    "parseSave\x20error:",
+    "getElementById",
+    "請輸入驗證碼",
+    "轉移碼格式錯誤！",
+    "請選擇版本",
+    "Send\x20Query:",
+    "Failed\x20to\x20Parse:\x20",
+    "119822xRdgJm",
+    "querySelector",
+    "https://bc.godfat.org/?seed=",
+    "value",
+    "textContent",
+    "2142152VYjDYm",
+    "test",
+    "href",
+    "80736hWAEVu",
   ];
-  _0x35fd = function () {
-    return _0x57bd4a;
+  _0x370d = function () {
+    return _0x307497;
   };
-  return _0x35fd();
+  return _0x370d();
 }
-(function (_0x165f91, _0x24bc0d) {
-  const _0x47cc74 = _0x2dbe,
-    _0x14cb82 = _0x165f91();
+(function (_0x583565, _0xc223a7) {
+  const _0x2d7c05 = _0x4cd2,
+    _0x285c90 = _0x583565();
   while (!![]) {
     try {
-      const _0x385f06 =
-        (parseInt(_0x47cc74(0x1d8)) / 0x1) *
-          (parseInt(_0x47cc74(0x1c5)) / 0x2) +
-        (parseInt(_0x47cc74(0x1c1)) / 0x3) *
-          (parseInt(_0x47cc74(0x1cb)) / 0x4) +
-        (parseInt(_0x47cc74(0x1be)) / 0x5) *
-          (-parseInt(_0x47cc74(0x1bd)) / 0x6) +
-        parseInt(_0x47cc74(0x1bf)) / 0x7 +
-        -parseInt(_0x47cc74(0x1da)) / 0x8 +
-        parseInt(_0x47cc74(0x1ca)) / 0x9 +
-        parseInt(_0x47cc74(0x1c9)) / 0xa;
-      if (_0x385f06 === _0x24bc0d) break;
-      else _0x14cb82["push"](_0x14cb82["shift"]());
-    } catch (_0x5ac13e) {
-      _0x14cb82["push"](_0x14cb82["shift"]());
+      const _0x8a27a0 =
+        (parseInt(_0x2d7c05(0x17f)) / 0x1) *
+          (parseInt(_0x2d7c05(0x18d)) / 0x2) +
+        parseInt(_0x2d7c05(0x181)) / 0x3 +
+        (parseInt(_0x2d7c05(0x173)) / 0x4) *
+          (parseInt(_0x2d7c05(0x179)) / 0x5) +
+        (parseInt(_0x2d7c05(0x195)) / 0x6) *
+          (parseInt(_0x2d7c05(0x184)) / 0x7) +
+        (-parseInt(_0x2d7c05(0x192)) / 0x8) *
+          (parseInt(_0x2d7c05(0x17b)) / 0x9) +
+        (-parseInt(_0x2d7c05(0x182)) / 0xa) *
+          (parseInt(_0x2d7c05(0x17d)) / 0xb) +
+        -parseInt(_0x2d7c05(0x172)) / 0xc;
+      if (_0x8a27a0 === _0xc223a7) break;
+      else _0x285c90["push"](_0x285c90["shift"]());
+    } catch (_0x2daa14) {
+      _0x285c90["push"](_0x285c90["shift"]());
     }
   }
-})(_0x35fd, 0xb4fad);
+})(_0x370d, 0xe5dcf);
+const axios = require("axios");
+function _0x4cd2(_0x2d3a95, _0x37f77f) {
+  const _0x370d03 = _0x370d();
+  return (
+    (_0x4cd2 = function (_0x4cd22c, _0x48199f) {
+      _0x4cd22c = _0x4cd22c - 0x171;
+      let _0x3083f8 = _0x370d03[_0x4cd22c];
+      return _0x3083f8;
+    }),
+    _0x4cd2(_0x2d3a95, _0x37f77f)
+  );
+}
 async function downloadFile() {
-  const _0x491a67 = _0x2dbe,
-    _0x2bbe0a = document["getElementById"]("account")[_0x491a67(0x1d7)],
-    _0x2a1295 = document[_0x491a67(0x1d9)]("password")[_0x491a67(0x1d7)],
-    _0x1439e7 = document["querySelector"](_0x491a67(0x1d1)),
-    _0x4b874c = document[_0x491a67(0x1d9)]("gv")[_0x491a67(0x1d7)],
-    _0x5501a0 = /^[A-Za-z0-9]{9}$/,
-    _0x51d419 = /^\d{4}$/;
-  if (!_0x2bbe0a || !_0x2a1295 || !_0x1439e7 || !_0x4b874c)
+  const _0x552fa6 = _0x4cd2,
+    _0x3d8088 = document[_0x552fa6(0x187)]("account")[_0x552fa6(0x190)],
+    _0x1026aa = document["getElementById"](_0x552fa6(0x196))[_0x552fa6(0x190)],
+    _0x255374 = document[_0x552fa6(0x18e)]("input[name=\x22cc\x22]:checked"),
+    _0x5489a1 = document["getElementById"]("gv")[_0x552fa6(0x190)],
+    _0x18c2cc = /^[A-Za-z0-9]{9}$/,
+    _0x60bd55 = /^\d{4}$/;
+  if (!_0x3d8088 || !_0x1026aa || !_0x255374 || !_0x5489a1)
     alert(
-      !_0x2bbe0a
-        ? _0x491a67(0x1cf)
-        : !_0x2a1295
-        ? "請輸入驗證碼"
-        : !_0x1439e7
-        ? "請選擇版本"
-        : _0x491a67(0x1d5)
+      !_0x3d8088
+        ? _0x552fa6(0x178)
+        : !_0x1026aa
+        ? _0x552fa6(0x188)
+        : !_0x255374
+        ? _0x552fa6(0x18a)
+        : _0x552fa6(0x180)
     );
   else
-    !_0x5501a0["test"](_0x2bbe0a) || !_0x51d419[_0x491a67(0x1c4)](_0x2a1295)
+    !_0x18c2cc[_0x552fa6(0x193)](_0x3d8088) ||
+    !_0x60bd55[_0x552fa6(0x193)](_0x1026aa)
       ? alert(
-          !_0x5501a0[_0x491a67(0x1c4)](_0x2bbe0a)
-            ? "轉移碼格式錯誤！"
-            : _0x491a67(0x1c6)
+          !_0x18c2cc[_0x552fa6(0x193)](_0x3d8088)
+            ? _0x552fa6(0x189)
+            : _0x552fa6(0x177)
         )
-      : console[_0x491a67(0x1d6)]("Send\x20Query:", {
-          account: _0x2bbe0a,
-          password: _0x2a1295,
-          cc: _0x1439e7[_0x491a67(0x1d7)],
-          gv: _0x4b874c,
+      : console[_0x552fa6(0x183)](_0x552fa6(0x18b), {
+          account: _0x3d8088,
+          password: _0x1026aa,
+          cc: _0x255374[_0x552fa6(0x190)],
+          gv: _0x5489a1,
         });
-  const _0xf56a59 = await axios["post"](
-      _0x491a67(0x1d0),
+  const _0x260362 = await axios[_0x552fa6(0x176)](
+      "http://localhost:3000/download",
       {
-        account: _0x2bbe0a,
-        password: _0x2a1295,
-        cc: _0x1439e7[_0x491a67(0x1d7)],
-        gv: _0x4b874c,
+        account: _0x3d8088,
+        password: _0x1026aa,
+        cc: _0x255374[_0x552fa6(0x190)],
+        gv: _0x5489a1,
       },
-      { responseType: _0x491a67(0x1ce) }
+      { responseType: "arraybuffer" }
     ),
-    _0x414221 = new Uint8Array(_0xf56a59[_0x491a67(0x1c7)]);
+    _0x21360a = new Uint8Array(_0x260362[_0x552fa6(0x185)]);
   try {
-    const _0x1400c4 = parseSave(_0x414221, _0x1439e7["value"]);
-    console[_0x491a67(0x1d6)]("rareGachaSeed:", _0x1400c4),
-      _0x1400c4 === undefined
-        ? (document[_0x491a67(0x1d9)](_0x491a67(0x1c0))[_0x491a67(0x1cd)] =
-            _0x491a67(0x1c2))
-        : ((document[_0x491a67(0x1d9)](_0x491a67(0x1c0))[_0x491a67(0x1cd)] =
-            _0x491a67(0x1c3) + _0x1400c4),
-          (window[_0x491a67(0x1db)]["href"] =
-            _0x491a67(0x1d2) +
-            _0x1400c4 +
-            _0x491a67(0x1c8) +
-            _0x1439e7[_0x491a67(0x1d7)]));
-  } catch (_0x3f92b6) {
-    console[_0x491a67(0x1cc)]("parseSave\x20error:", _0x3f92b6),
-      (document[_0x491a67(0x1d9)]("seed")[_0x491a67(0x1cd)] =
-        _0x491a67(0x1d4) + _0x3f92b6[_0x491a67(0x1d3)]);
+    const _0x4d6c1e = parseSave(_0x21360a, _0x255374[_0x552fa6(0x190)]);
+    console["log"](_0x552fa6(0x17c), _0x4d6c1e),
+      _0x4d6c1e === undefined
+        ? (document["getElementById"]("seed")[_0x552fa6(0x191)] =
+            _0x552fa6(0x17e))
+        : ((document[_0x552fa6(0x187)](_0x552fa6(0x175))[_0x552fa6(0x191)] =
+            "Seed:\x20" + _0x4d6c1e),
+          (window["location"][_0x552fa6(0x194)] =
+            _0x552fa6(0x18f) +
+            _0x4d6c1e +
+            _0x552fa6(0x171) +
+            _0x255374[_0x552fa6(0x190)]));
+  } catch (_0x247dec) {
+    console[_0x552fa6(0x17a)](_0x552fa6(0x186), _0x247dec),
+      (document[_0x552fa6(0x187)]("seed")[_0x552fa6(0x191)] =
+        _0x552fa6(0x18c) + _0x247dec[_0x552fa6(0x174)]);
   }
 }
